7825f5c045d51cd32d96b6b37c21f1798579f405
Kim <72332310+kimnle@users.noreply.github.com>
2024-10-19 11:23:50 +1100

    feat: user sign up route

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -17,4 +17,17 @@ router.get("/", (request, response) => {
     });
 });
 
+// POST localhost:3000/users/signup
+router.post("/signup", async(request, response) => {
+    let receiveUserData = request.body;
+
+    receiveUserData.password = "EncryptedPassword";
+
+    // pretend we have written receiveUserData into a DB
+    let placeholderDatabaseResult = {...receiveUserData};
+
+    // placeholderDatabaseResult is a placeholder for actually making a new user in a database
+    response.json(receiveUserData);
+});
+
 module.exports = router;
